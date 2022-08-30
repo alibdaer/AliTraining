f89ad9b9da1790bef869335ee654488201621e88
alibdaer96 <ali.bdear@menaitech.com>
2022-08-30 13:46:17 +0300

    edit

--- src/test/java/automation/ChromeDriverDemo.java
+++ src/test/java/automation/ChromeDriverDemo.java
@@ -7,12 +7,9 @@ public class ChromeDriverDemo {
     public static void main(String[] args) {
 
         String ChromePath = "resources/chromedriver.exe";
-
         System.setProperty("webdriver.chrome.driver",ChromePath);
-
         ChromeDriver driver2 = new ChromeDriver();
         driver2.navigate().to("http://google.com");
-
 
     }
 }
---
+++ src/test/java/automation/TestNGDemo.java
@@ -0,0 +1,35 @@
+package automation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+    ChromeDriver driver2;
+
+    @BeforeTest
+    public void openURL(){
+        String ChromePath = "resources/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver",ChromePath);
+        driver2 = new ChromeDriver();
+        driver2.navigate().to("http://google.com");
+    }
+
+    @Test
+    public void userLogin(){
+        System.out.println(driver2.getCurrentUrl());
+    }
+
+    @Test
+    public void userCanBuy(){
+        System.out.println(driver2.getTitle());
+    }
+
+    @AfterTest
+    public void lastTest(){
+        driver2.quit();
+    }
+
+}
